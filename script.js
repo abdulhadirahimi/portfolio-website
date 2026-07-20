6fc2f25a0171cb6159874aae3be02c271d4c8c70
Abdulhadi <abhadirahimi623@gmail.com>
2026-07-20 23:35:23 +0430

    Initialize JavaScript for portfolio

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Portfolio Loaded Successfully");
+
+const title = document.getElementById("title");
+
+console.log(title);
+
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", function () {
+
+    console.log("Button Clicked");
+
+});
